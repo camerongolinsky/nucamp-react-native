3d5c11d20ee027afa6d2d9a8a2c7802698914266
Cameron Golinsky <camerongolinsky@gmail.com>
2020-11-30 23:44:55 -0800

    swipe and delete favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -173,4 +173,9 @@ export const postFavorite = campsiteId => dispatch => {
 export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
-})
+});
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId 
+});
