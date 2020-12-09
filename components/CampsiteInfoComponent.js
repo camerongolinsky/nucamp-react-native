a07a70108c10d6a7718fda54530f52161725e2e8
Cameron Golinsky <camerongolinsky@gmail.com>
2020-12-08 22:32:21 -0800

    share

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -9,6 +9,7 @@ import {
   StyleSheet,
   Alert,
   PanResponder,
+  Share
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -75,7 +76,17 @@ function RenderCampsite(props) {
     },
   });
 
-  const { campsite } = props;
+  const shareCampsite = (title, message, url) => {
+    Share.share({
+      title,
+      message: `${title}: ${message} ${url}`,
+      url
+    },
+    { 
+      dialogTitle: 'Share ' + title
+    })  
+  };
+
   if (campsite) {
     return (
       <Animatable.View
@@ -111,6 +122,15 @@ function RenderCampsite(props) {
               reverse
               onPress={() => props.onShowModal()}
             />
+            <Icon 
+              name='share'
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() => shareCampsite(campsite.name, campsite.description,
+                baseUrl + campsite.image)
+            />  
           </View>
         </Card>
       </Animatable.View>
